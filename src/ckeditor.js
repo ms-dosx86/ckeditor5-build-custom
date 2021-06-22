88bf9ce93cc3ae15d06aa0b5679bc8da747fdb09
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2021-06-22 20:18:29 +0700

    add missed plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,6 +5,7 @@ import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKFinderPlugin from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImagePlugin from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
 import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
@@ -12,10 +13,16 @@ import ImageCaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload';
+import IndentPlugin from '@ckeditor/ckeditor5-indent/src/indent';
 import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
 import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
 import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import MediaEmbedPlugin from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import PasteFromOfficePlugin from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import TablePlugin from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbarPlugin from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformationPlugin from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
 export default class ClassicEditor extends ClassicEditorBase { }
 
@@ -37,6 +44,13 @@ ClassicEditor.builtinPlugins = [
 	ListPlugin,
 	ParagraphPlugin,
 	CloudServices,
+	CKFinderPlugin,
+	IndentPlugin,
+	MediaEmbedPlugin,
+	PasteFromOfficePlugin,
+	TablePlugin,
+	TableToolbarPlugin,
+	TextTransformationPlugin,
 ];
 
 ClassicEditor.defaultConfig = {
@@ -49,8 +63,14 @@ ClassicEditor.defaultConfig = {
 			'link',
 			'bulletedList',
 			'numberedList',
+			'|',
+			'indent',
+			'outdent',
+			'|',
 			'uploadImage',
 			'blockQuote',
+			'insertTable',
+			'mediaEmbed',
 			'undo',
 			'redo'
 		]
@@ -61,6 +81,13 @@ ClassicEditor.defaultConfig = {
 			'imageStyle:side',
 			'|',
 			'imageTextAlternative'
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells'
 		]
 	},
 	language: 'ru'
